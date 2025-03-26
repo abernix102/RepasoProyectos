c53a85195dddfbffb573e4caa4eac0ec54a89698
Nahuel <nahuelo841@gmail.com>
2025-03-26 08:42:00 -0300

    changes

--- js/app.js
+++ js/app.js
@@ -1,30 +1,54 @@
-const listaCursos = document.querySelector("#lista-cursos")
-const carrito = document.querySelector('#carrito')
-const contenedorCarrito = document.querySelector('#ista-carrito tbody');
-const vaciarCarrito = document.querySelector('#vaciar-carrito')
+const listaCursos = document.querySelector("#lista-cursos");
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarrito = document.querySelector("#vaciar-carrito");
 
-
-cargarEvenlistener()
+cargarEvenlistener();
 
 let carritoDeCursos = [];
 
 function cargarEvenlistener() {
-     listaCursos.addEventListener('click', agregarCurso)
+  listaCursos.addEventListener("click", agregarCurso);
 }
-
 
 function agregarCurso(e) {
-     e.preventDefault();
-     if(e.target.classList.contains('agregar-carrito')){
-          const cursoSeleccionado = e.target.parentElement.parentElement;
-          leerDatosCurso(cursoSeleccionado)
-     }
+  e.preventDefault();
+  if (e.target.classList.contains("agregar-carrito")) {
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
+  }
 }
 
-function leerDatosCurso(curso){
-     // console.log(curso)
-     infoCurso = {
-          img: curso.querySelector("img").src
-     }
-     console.log(infoCurso.img)
+function leerDatosCurso(curso) {
+  const infoCurso = {
+    img: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 0
+  };
+  
+  carritoDeCursos = [...carritoDeCursos, infoCurso];
+  cargarCarrito();
+}
+
+function cargarCarrito() {
+  carritoDeCursos.forEach(curso => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>
+    <img src=${curso.img} />
+    </td>
+     <td>
+        ${curso.titulo}
+     </td>
+     <td>
+     ${curso.precio}
+     </td>
+     <td>
+     ${curso.cantidad}
+     </td>
+     `;
+    contenedorCarrito.appendChild(row);
+  });
 }
